a49e4693a2058ff8ed5bbd809a8f1af2918bd9b2
ghostchinchilla <beca.mckay@yahoo.com>
2023-11-16 12:15:59 -0500

    added routes

--- boggle/app.py
+++ boggle/app.py
@@ -16,5 +16,26 @@ def homepage():
     return render_template("index.html", board=board,
                            highscore=highscore, nplays=nplays)
 
+@app.route("/check-word")
+def check_word():
+    """Check if word is in dictionary."""
 
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
+
+    return jsonify({'result': response})
+
+@app.route("/post-score", methods=["POST"])
+def post_score():
+    """Receive score, update nplays, update high score if appropriate."""
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord=score > highscore)
 
