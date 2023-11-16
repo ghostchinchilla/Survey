45708bc6153d545b3852b0cd565ada9c7d60066f
ghostchinchilla <beca.mckay@yahoo.com>
2023-11-16 12:02:12 -0500

    added home route

--- boggle/app.py
+++ boggle/app.py
@@ -1,7 +1,20 @@
+from flask import Flask, request, render_template, jsonify, session
 from boggle import Boggle
+
+app = Flask (__name__)
+app.config["SECRET_KEY"] = "fdfgkjtjkkg45yfdb"
 
 boggle_game = Boggle()
 
-app = Flask (__name__)
+@app.route("/")
+def homepage():
+    board = boggle_game.make_board()
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
 
-app.config["SECRET_KEY"] = "fdfgkjtjkkg45yfdb"
+    return render_template("index.html", board=board,
+                           highscore=highscore, nplays=nplays)
+
+
+
